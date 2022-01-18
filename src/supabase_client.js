6e9d64b1dbe2724348230b2a86d1ac1d2e91232d
Erick G. Alfaro <ericks.network@gmail.com>
2022-01-17 20:47:46 -0500

    keys to env

--- src/supabase_client.js
+++ src/supabase_client.js
@@ -1,6 +1,6 @@
 import {createClient} from '@supabase/supabase-js'
 
 export const supabase = createClient(
-     "https://jhhhwqxvgiwiozdkfran.supabase.co",
-     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDM0NTkyNSwiZXhwIjoxOTM1OTIxOTI1fQ.SX7UZzDGbQj4KLxdu0g0j3GDXv7U8ZyD6s6S0nhb5tY"
+     process.env.REACT_APP_SUPABASE_URL,
+     process.env.REACT_APP_SUPABASE_KEY
 )
